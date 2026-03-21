fa61f2db6101b10afbb2021fce63644d52d5bf3d
vakiltrack-backend <whitesheepenglish@gmail.com>
2026-03-21 14:22:31 -0400

    fix firebase env config

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
